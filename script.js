21f76d44c8b3dcfe55987c701efd80a05fabe60f
Antônio de Pádua <toninhoport@gmail.com>
2022-12-21 19:06:04 -0300

    update

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-
-const display = document.querySelector(".display-info")
-const procurarBTN = document.querySelector(".procurar")
-procurarBTN.addEventListener("click",()=>{
-  checkCep()
-})
-const showData = (data) =>{
-  for(const field in data){
-    console.log(field)
-    if(document.querySelector("#"+field)){
-      document.querySelector("#"+field).value = data[field]
+const display = document.querySelector(".display-info");
+const procurarBTN = document.querySelector(".procurar");
+procurarBTN.addEventListener("click", () => {
+  checkCep();
+});
+const showData = (data) => {
+  for (const field in data) {
+    console.log(field);
+    if (document.querySelector("#" + field)) {
+      document.querySelector("#" + field).value = data[field];
     }
-   
   }
-  
+};
+function checkCep() {
+  const cep = document.querySelector("#cep");
+  let mainCEP = cep.value.replace("-", "");
+  const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  };
+  let fetchAPI = fetch(`https://viacep.com.br/ws/${mainCEP}/json/`, options)
+    .then((res) => {
+      if (!res.ok) {
+        throw error("error");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      showData(data);
+    });
 }
-function checkCep(){
-    const cep = document.querySelector("#cep")
-    let mainCEP = cep.value.replace("-","")
-    const options = {
-      method : 'GET',
-      mode: 'cors',
-      cache: 'default'
-  }
-  let fetchAPI = fetch(`https://viacep.com.br/ws/${mainCEP}/json/`,options)
-  .then(res => {
-    if(!res.ok){
-      throw error('error')
-    }
-    return res.json()
-  })
-  .then(data => {
-    showData(data)
-  })
-}
- 
